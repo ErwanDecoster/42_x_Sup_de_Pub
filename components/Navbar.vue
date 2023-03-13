5b37c4abc5e4802da58b5538d683f563d1f8dc23
Dinho <darksheep42350@gmail.com>
2023-03-13 20:41:01 +0100

    indexv0.2

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed h-32 w-full flex">
+  <div class="fixed h-32 w-full flex z-40">
     <div class="flex justify-between max-w-screen-xl m-auto w-full">
       <img
         class="h-16"
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <section class="background w-full h-[95vh] flex justify-center items-center">
-      <img class="h-2/5 absolute -right-20 -top-2 -rotate-90" src="../public/halfcircle.svg" alt="">
+      <img
+        class="h-3/5 absolute -right-0 -translate-y-80 z-0"
+        src="../public/halfcirclegreen+dot.svg"
+        alt=""
+      >
+      <!-- <img
+        class=""
+        src="../public"
+        alt=""
+      > -->
       <div class="z-10">
         <div class="custom-shadow h-0 w-0 rounded-full bg-maastricht-blue absolute top-1/2 left-1/2 -z-10" />
         <h1 class="text-white text-8xl relative -translate-y-1/2">
@@ -10,11 +19,21 @@
         </h1>
         <div class="flex flex-col items-center gap-8 mt-20">
           <p class="text-white">
-           Du <b>31.05</b> au <b>28.07</b>
+            Du <b>31.05</b> au <b>28.07</b>
           </p>
           <div class="flex gap-5">
-            <NuxtLink to="" class="button-secondary">Programmation</NuxtLink>
-            <NuxtLink to="" class="button-primary-two">Billeterie</NuxtLink>
+            <NuxtLink
+              to=""
+              class="button-secondary"
+            >
+              Programmation
+            </NuxtLink>
+            <NuxtLink
+              to=""
+              class="button-primary-two"
+            >
+              Billeterie
+            </NuxtLink>
           </div>
         </div>
       </div>
