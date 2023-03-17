e120957b7515ef8053edbcf10983ea1509ad16ab
Aymeric Jakubczyk <ajakubcz@z4r5p1.42lyon.fr>
2023-03-17 18:18:58 +0100

    debut de l'amelioration du footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col items-end w-full z-10">
+  <div class="flex flex-col w-full z-10">
     <div class="flex bg-maximum-blue-green w-full">
       <div class="flex lg:items-center flex-col m-auto lg:flex-row justify-between gap-10 max-w-screen-2xl w-full py-12 px-6">
         <div>
@@ -22,8 +22,8 @@
       </div>
     </div>
     <div class="relative flex bg-black w-full pb-52">
-      <div class="grid m-auto md:grid-cols-2 sm:grid-cols-1 xl:grid-cols-4 justify-between gap-8 max-w-screen-2xl w-full py-12 px-6">
-        <div class="flex flex-col h-40 justify-between">
+      <div class="grid m-auto md:grid-cols-2 grid-cols-1 xl:grid-cols-4 justify-between gap-8 max-w-screen-2xl w-full py-12 px-6">
+        <div class="flex flex-col md:order-1 order-3 h-40 justify-between">
           <p class="whitespace-nowrap text-white font-bold text-2xl">
             LES NUITS DE FOURVIÈRE
           </p>
@@ -34,7 +34,7 @@
             INFORMATIONS : 04 72 57 15 40<br>BILLETTERIE : 04 72 32 00 00
           </p>
         </div>
-        <div>
+        <div class="flex flex-col md:order-2 order-2">
           <p class="whitespace-nowrap text-white font-bold text-2xl mb-5">
             RÉSEAUX SOCIAUX
           </p>
@@ -81,18 +81,18 @@
             </NuxtLink>
           </div>
         </div>
-        <div>
+        <div class="flex flex-col md:order-3 order-4">
           <p class="whitespace-nowrap text-white font-bold text-2xl mb-5">
             PLAN DU SITE
           </p>
           <NuxtLink
-            to=""
+            to="/"
             class="button-simple text-base font-bold normal-case p-0"
           >
             Accueil
           </NuxtLink>
           <NuxtLink
-            to=""
+            to="/about_us"
             class="button-simple text-base font-bold normal-case p-0"
           >
             Qui sommes-nous ?
@@ -104,19 +104,19 @@
             Infos pratiques
           </NuxtLink>
           <NuxtLink
-            to=""
+            to="/infos"
             class="button-simple text-base font-bold normal-case p-0"
           >
             Programmation
           </NuxtLink>
           <NuxtLink
-            to=""
+            to="/programming_and_ticketing"
             class="button-simple text-base font-bold normal-case p-0"
           >
             Billetterie
           </NuxtLink>
         </div>
-        <div class="flex flex-col gap-5">
+        <div class="flex flex-col md:order-4 order-1 gap-5">
           <p class="text-white font-bold text-2xl">
             NEWSLETTER
           </p>
@@ -144,6 +144,17 @@
         src="@/src/logo_square.svg"
         alt=""
       >
+      <img
+        class="absolute w-40 h-16 sm:w-72 sm:h-28 md:bottom-36 sm:bottom-28 md:left-96 sm:left-60 left-56 bottom-20"
+        src="@/src/logo_metropole.png"
+        alt=""
+      >
+    </div>
+    <div class="flex bg-alizarin-crimson w-[3000px]">
+      <p class="absolute text-white p-2 left-80">
+        CE & Collectivités - Mentions légales - Presse - Règlement du festival & CGV - Technique - 
+        Politique de confidentialité - Cookies
+      </p>
     </div>
   </div>
 </template>
